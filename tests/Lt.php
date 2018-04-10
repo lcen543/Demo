5f10692671a0b2793534422caf636c48476624ce
lcen543 <lcen543@163.com>
2018-04-10 20:09:26 +0800

    123

---
+++ tests/Lt.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the webmozart/assert package.
+ *
+ * (c) Bernhard Schussek <bschussek@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Lichen\TEST;
+
+
+class Lt
+{
+    private static $resource;
+
+    public function __construct()
+    {
+
+    }
+}
